6531fa38abda0a4075ac1ad673680a8c3ed9ac56
AkshayKumar0508 <akshay.kumar@ndus.edu>
2025-03-21 23:15:20 -0500

    Made changes to implicit intent as suggested

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -13,19 +13,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonExplicit = findViewById<Button>(R.id.explicitButton)
-        val buttonImplicit = findViewById<Button>(R.id.implicitButton)
+        //val buttonExplicit = findViewById<Button>(R.id.explicitButton)
+        //val buttonImplicit = findViewById<Button>(R.id.implicitButton)
+        val explicitButton = findViewById<Button>(R.id.explicitButton)
+        val implicitButton = findViewById<Button>(R.id.implicitButton)
+        val viewImageActivityBtn: Button = findViewById(R.id.ViewImageActivityButton)
+
 
         // Button to start second activity explicitly
-        buttonExplicit.setOnClickListener {
+        explicitButton.setOnClickListener {
             val explicitIntent = Intent(this, SecondActivity::class.java)
             startActivity(explicitIntent)
         }
 
         // Button to start second activity implicitly
-        buttonImplicit.setOnClickListener {
-            val implicitIntent = Intent("com.example.ACTION_VIEW_SECOND_ACTIVITY")
+        implicitButton.setOnClickListener {
+           // val implicitIntent = Intent("com.example.ACTION_VIEW_SECOND_ACTIVITY")
+           val implicitIntent = Intent(this, SecondActivity::class.java)
             startActivity(implicitIntent)
+        }
+        // Button to start third activity
+        viewImageActivityBtn.setOnClickListener {
+            val intent = Intent(this, ImageActivity::class.java)
+            startActivity(intent)
         }
     }
 }
